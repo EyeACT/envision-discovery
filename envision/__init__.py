ed1ba56c63b950a27351280562fad8727eb6f433
Jamey ONeill <jimnoneill@gmail.com>
2026-03-03 19:54:45 -0800

    refactor: use envision-classifier package instead of bundled classifier

--- envision/__init__.py
+++ envision/__init__.py
@@ -1,34 +1,16 @@
 """
-ENVISION: Eye Imaging Dataset Classifier
+ENVISION Discovery: Eye Imaging Dataset Discovery Pipeline
 
-A 4-class SetFit classifier for detecting eye imaging datasets:
-  - EYE_IMAGING: Actual eye imaging datasets (fundus, OCT, OCTA, etc.)
-  - EYE_SOFTWARE: Code, models, tools for eye imaging
-  - EDGE_CASE: Eye research papers, reviews, borderline items
-  - NEGATIVE: Unrelated domains
+Uses envision-classifier to find eye imaging datasets across repositories.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.1.0"
 __author__ = "James O'Neill"
 
-from .classifier import EyeImagingClassifier
-
-# Backward-compatible training data exports
-from .classifier import (
-    POSITIVE_EXAMPLES,
-    SOFTWARE_EXAMPLES,
-    EDGE_CASES,
-    NEGATIVE_EXAMPLES,
-    LABELS,
-    MODEL_NAME,
-)
+# Re-export classifier from envision-classifier package
+from envision_classifier import EyeImagingClassifier, LABELS
 
 __all__ = [
     "EyeImagingClassifier",
-    "POSITIVE_EXAMPLES",
-    "SOFTWARE_EXAMPLES",
-    "EDGE_CASES",
-    "NEGATIVE_EXAMPLES",
     "LABELS",
-    "MODEL_NAME",
 ]
